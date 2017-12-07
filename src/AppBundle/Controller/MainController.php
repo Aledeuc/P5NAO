9f3ea0e0ea4069bcfc8ed72f9053e30c1a576360
pywako <wongaliceyy@gmail.com>
2017-12-07 09:21:03 +0100

    initialisation : views de base, controller, stylesheets

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -1,4 +1,6 @@
 <?php
+/**
+ */
 
 namespace AppBundle\Controller;
 
@@ -6,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class MainController extends Controller
 {
     /**
      * @Route("/", name="homepage")
@@ -14,8 +16,15 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('main/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/mentions", name="mentions")
+     */
+    public function mentionAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('main/mentions.html.twig', []);
     }
 }
