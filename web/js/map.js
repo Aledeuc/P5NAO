085bf53ac3b08ffc52e01a697539f8a766654726
pywako <wongaliceyy@gmail.com>
2017-12-16 21:56:57 +0100

    ajout script js affichage pins multiple

--- web/js/map.js
+++ web/js/map.js
@@ -4,51 +4,49 @@ $(function () {
 
 function initMap() {
     var centreFrance = {lat: 46.498392, lng: 2.610353};
-    var response = '{{ response }}';
     map = new google.maps.Map(document.getElementById('map'), {
         center: centreFrance,
         zoom: 5
     });
-    var marker = new google.maps.Marker({
-        position: response,
-        map: map
-    });
-    map.data.setStyle(function (feature) {
-        var magnitude = 2;
-        return {
-            icon: getCircle(magnitude)
+    var markers =[];
+    var locations = [
+        {title: 'oiseau1', lat: 46.498392, lng: 2.610353},
+        {title: 'oiseau2', lat: 48.8615, lng: 2.34706}
+    ];
+
+    var largeInfowindow = new google.maps.InfoWindow({});
+    var bounds = new google.maps.LatLngBounds();
+
+    for (var i = 0; i < locations.length; i++) {
+        var position = {
+            lat: locations[i].lat,
+            lng: locations[i].lng
         };
-    });
+        var title = locations[i].title;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        markers.push(marker);
+        bounds.extend(marker.position);
+        marker.addListener('click', function(){
+            populateInfoWindow(this, largeInfowindow);
+        });
+    }
+    map.fitBounds(bounds);
 
-}
-
-function getCircle(magnitude) {
-    var circle = {
-        path: google.maps.SymbolPath.CIRCLE,
-        fillColor: 'red',
-        fillOpacity: .2,
-        scale: magnitude,
-        strokeColor: 'white',
-        strokeWeight: .5
-
-    };
-    return circle;
-}
-
-
-function downloadUrl(url, callback) {
-    var request = window.ActiveXObject ?
-        new ActiveXObject('Microsoft.XMLHTTP') :
-        new XMLHttpRequest;
-
-    request.onreadystatechange = function () {
-        if (request.readyState == 4) {
-            request.onreadystatechange = doNothing;
-            callback(request, request.status);
+    function populateInfoWindow(marker, infowindow){
+        if(infowindow.marker != marker) {
+            infowindow.marker = marker;
+            infowindow.setContent('<div>' + marker.title + '</div>');
+            infowindow.open(map, marker);
+            infowindow.addListener('closeclick', function(){
+                infowindow.setMarket(null);
+            });
         }
-    };
-
-    request.open('GET', url, true);
-    request.send(null);
+    }
 }
 
