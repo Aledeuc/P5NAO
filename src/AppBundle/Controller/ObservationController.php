2d3905b127667772952c7f8a840f0f92991d08cd
pywako <wongaliceyy@gmail.com>
2017-12-22 11:22:41 +0100

    ajout fichier base vue controller observation

---
+++ src/AppBundle/Controller/ObservationController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class ObservationController extends Controller
+{
+    /**
+     * @Route("/add", name="addObservation")
+     */
+    public function addAction()
+    {
+
+    }
+}
