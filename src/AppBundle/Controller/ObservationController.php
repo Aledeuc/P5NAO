b9bc80470269f3b1714ba387a5daead9430ce703
pywako <wongaliceyy@gmail.com>
2018-01-03 14:01:24 +0100

    test entity Type pour taxref dans addObservationType form

--- src/AppBundle/Controller/ObservationController.php
+++ src/AppBundle/Controller/ObservationController.php
@@ -6,7 +6,11 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\Observation;
+use AppBundle\Entity\Taxref;
 use AppBundle\Form\Type\AddObservationType;
+use AppBundle\Form\Type\ImageObservationType;
+use AppBundle\Services\ObservationManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,12 +21,24 @@ class ObservationController extends Controller
 {
     /**
      * @param Request $request
+     * @param ObservationManager $observationManager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      * @Route("/add", name="addObservation")
-     *
+     * @Method({"GET","POST"})
      */
     public function addAction(Request $request)
     {
+        /**
+        * if(!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')){
+        *     throw $this->createAccessDeniedException('Vous devez être connecté pour ajouter une observation');
+        * }
+        */
         $observation = new Observation();
+
+
+        $classe = $this->getDoctrine()->getManager()->getRepository('AppBundle:Taxref')->findOneBy(array('classe' => 'aves'));
+
+        //$observation->setTaxrefs($test);
 
         $form = $this->createForm(AddObservationType::class, $observation);
         $form->handleRequest($request);
--- src/AppBundle/Form/Type/AddObservationType.php
+++ src/AppBundle/Form/Type/AddObservationType.php
@@ -4,7 +4,11 @@
 
 namespace AppBundle\Form\Type;
 
+use AppBundle\AppBundle;
 use AppBundle\Entity\Observation;
+use AppBundle\Entity\Taxref;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -18,47 +22,77 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AddObservationType extends AbstractType
 {
-       public function buildForm(FormBuilderInterface $builder, array $options)
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('observationDate', DateType::class, [
-                'label'=> 'Date d\'observation',
-                'widget'=> 'single_text',
+                'widget' => 'single_text',
                 'format' => 'dd/MM/yyyy',
-                'html5'=> false,
-                'attr' =>['class' => 'form-control datepicker']
+                'html5' => false,
+                'attr' => ['class' => 'form-control datepicker']
             ])
-            ->add('taxrefs', CollectionType::class, [
-                'entry_type'=> TaxrefObservationType::class,
+            ->add('taxref', EntityType::class, [
+                'class' => Taxref::class,
+                'query_builder' => function (EntityRepository $er) {
+                return $er->createQueryBuilder('u')
+                    ->orderBy('u.famille', 'ASC');
+                },
+                'choice_label' => 'famille'
             ])
             ->add('observationNumber', IntegerType::class, [
                 'label' => 'nombre d\'oiseaux'
             ])
             ->add('observationLatitude', NumberType::class, [
-                'attr' =>['placeholder' => 'latitude']
+                'attr' => ['placeholder' => 'latitude']
             ])
-            ->add('observationLongitude',NumberType::class, [
-                'attr' =>['placeholder' => 'longitude']
+            ->add('observationLongitude', NumberType::class, [
+                'attr' => ['placeholder' => 'longitude']
             ])
             ->add('observationEnvironment', ChoiceType::class, [
-                'label' => 'cadre'
+                'label' => 'cadre',
+                'choices' => array(
+                    'Ville' => Observation::ENVIRONMENT_CITY,
+                    'Forêt' => Observation::ENVIRONMENT_FOREST,
+                    'Jardin' => Observation::ENVIRONMENT_GARDEN,
+                    'Lac' => Observation::ENVIRONMENT_LAKE,
+                    'Mer' => Observation::ENVIRONMENT_SEA,
+                    'Montagne' => Observation::ENVIRONMENT_MOUNTAIN,
+                ),
+                'attr' => [
+                    'class' => 'mdb-select',
+                ],
             ])
             ->add('observationClimat', ChoiceType::class, [
-                'label' => 'climat'
+                'label' => 'climat',
+                'choices' => array(
+                    'Soleil' => Observation::CLIMATE_SUN,
+                    'Soleil et nuage' => Observation::CLIMATE_SUNCLOUD,
+                    'Nuageux' => Observation::CLIMATE_CLOUD,
+                    'Pluie' => Observation::CLIMATE_RAIN,
+                    'Neige' => Observation::CLIMATE_SNOW,
+                ),
+                'attr' => ['class' => 'mdb-select',
+                ],
             ])
-            ->add('observationImages', CollectionType::class, [
-                'entry_type' => ImageObservationType::class
-            ])
+            ->add('observationImages', CollectionType::class,[
+                'entry_type' =>ImageObservationType::class,
+            ] )
             ->add('observationComment', TextareaType::class, [
-
                 'label' => 'commentaires'
-            ])
-        ;
+            ]);
     }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults( array(
-            'data_class' => Observation::class
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Observation'
         ));
     }
 }
