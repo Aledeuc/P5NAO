f147ebf857995f982771c66bef6baf3410d27da9
pywako <wongaliceyy@gmail.com>
2018-01-09 22:00:50 +0100

    ajout de markers par défaut

--- web/js/map_marker.js
+++ web/js/map_marker.js
@@ -3,10 +3,10 @@
 })();
 
     function initMap() {
-        var centreFrance = {lat: 46.498392, lng: 2.610353};
+        var centreFrance = {lat: 46.1368447, lng: -2.4363844};
         var map = new google.maps.Map(document.getElementById('map'), {
             center: centreFrance,
-            zoom: 5,
+            zoom: 6
         });
 
         var markers =[];
@@ -14,34 +14,36 @@
         var elt = document.getElementById('json_data');
         var locations = elt.innerText || elt.textContent;
         locations = JSON.parse(locations);
-        if(!locations == ""){
-
-            var largeInfowindow = new google.maps.InfoWindow({});
-            var bounds = new google.maps.LatLngBounds();
-
-            for (var i = 0; i < locations.length; i++) {
-                var position = {
-                    lat: locations[i].lat,
-                    lng: locations[i].lng
-                };
-                var nom = locations[i].nom;
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: position,
-                    title: nom,
-                    animation: google.maps.Animation.DROP,
-                    id: i
-                });
-                markers.push(marker);
-                bounds.extend(marker.position);
-                marker.addListener('click', function(){
-                    populateInfoWindow(this, largeInfowindow);
-                });
-            }
-            map.fitBounds(bounds);
-        }else{
-
+        if(locations ==""){
+            locations = [
+                {nom: 'oiseau1', lat: 46.498392, lng: 2.610353},
+                {nom: 'oiseau2', lat: 48.8615, lng: 2.34706}
+            ]
         }
+
+        var largeInfowindow = new google.maps.InfoWindow({});
+        var bounds = new google.maps.LatLngBounds();
+
+        for (var i = 0; i < locations.length; i++) {
+            var position = {
+                lat: locations[i].lat,
+                lng: locations[i].lng
+            };
+            var nom = locations[i].nom;
+            var marker = new google.maps.Marker({
+                map: map,
+                position: position,
+                title: nom,
+                animation: google.maps.Animation.DROP,
+                id: i
+            });
+            markers.push(marker);
+            bounds.extend(marker.position);
+            marker.addListener('click', function(){
+                populateInfoWindow(this, largeInfowindow);
+            });
+        }
+        map.fitBounds(bounds);
 
         function populateInfoWindow(marker, infowindow) {
             if (infowindow.marker != marker) {
